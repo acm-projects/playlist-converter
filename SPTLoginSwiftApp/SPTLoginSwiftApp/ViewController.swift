3834eb63cfc5d05fdf068e8bfb209f4424de7d84
Vihas Gowreddy <dingoose@cometnet-10-21-58-191.utdallas.edu>
2019-04-24 19:09:28 -0500

    Current Backend

---
+++ SPTLoginSwiftApp/SPTLoginSwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPTLoginSwiftApp
+//
+//  Created by Vihas Gowreddy on 4/16/19.
+//  Copyright © 2019 Vihas Gowreddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SPTLoginSwiftApp/SPTLoginSwiftAppTests/SPTLoginSwiftAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SPTLoginSwiftAppTests.swift
+//  SPTLoginSwiftAppTests
+//
+//  Created by Vihas Gowreddy on 4/16/19.
+//  Copyright © 2019 Vihas Gowreddy. All rights reserved.
+//
+
+import XCTest
+@testable import SPTLoginSwiftApp
+
+class SPTLoginSwiftAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
